44b21cfc12451378531cbd7e1cb9b20778c3686f
Maestro2496 <agossou400@gmail.com>
2022-07-03 23:52:53 +0200

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,22 @@
-import Image from "next/image";
+import React, {useRef} from "react";
+
+import Banner from "../components/Banner";
+import Header from "../components/Header";
 import InfiniteScroll from "../components/InfiniteScroll";
-import styles from "../styles/Home.module.css";
+import Footer from "../components/Footer";
 
-export default function Home() {
+// All requests made with the client will be authenticated
+
+export default function Search() {
+  const ref = useRef();
   return (
-    <div className={styles.container}>
-      <InfiniteScroll />
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <>
+      <Header ref={ref} />
+      <Banner ref={ref} />
+      <div className="max-w-6xl mx-auto px-4 py-4">
+        <InfiniteScroll searchTerm="house" />
+      </div>
+      <Footer />
+    </>
   );
 }
